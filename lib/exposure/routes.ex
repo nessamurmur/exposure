dbbbbc7462094532f96688d770ec95df4e57b4f1
Nate West <natescott.west@gmail.com>
2014-08-27 17:04:51 -0500

    defroutes macro

--- lib/exposure/routes.ex
+++ lib/exposure/routes.ex
@@ -1,10 +1,26 @@
 defmodule Exposure.Routes do
 
+  defmacro __using__(_opts) do
+    quote do
+      import Exposure.Routes
+    end
+  end
+
   for verb <- ["GET", "POST", "PUT", "PATCH", "DELETE"] do
     method = verb |> String.downcase |> String.to_atom
 
     def unquote(method)(path, params, do: func) do
       %{method: unquote(method), path: path, params: params, func: func}
+    end
+  end
+
+  defmacro defroutes(name, routes) do
+    method = name |> String.downcase |> String.to_atom
+
+    quote do
+      def unquote(method)() do
+        unquote(routes)
+      end
     end
   end
 end
--- test/exposure/routes_test.exs
+++ test/exposure/routes_test.exs
@@ -26,4 +26,22 @@ defmodule Exposure.RoutesTest do
     expected = %{method: :delete, path: "/", params: [], func: nil}
     assert expected == Routes.delete "/", [], do: nil
   end
+
+  test "defroutes" do
+    expected = [%{method: :get, path: "/", params: [], func: nil},
+                %{method: :post, path: "/", params: [], func: nil}]
+
+
+
+    assert HomeRoutes.home == expected
+  end
+end
+
+defmodule HomeRoutes do
+  use Exposure.Routes
+
+  defroutes "home", [
+    get("/", [], do: nil),
+    post("/", [], do: nil)
+  ]
 end
