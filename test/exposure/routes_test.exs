9adb412d5f1f1147342a041d627ea472f7a91f60
Nate West <natescott.west@gmail.com>
2014-08-27 17:04:03 -0500

    Clean up tests

--- test/exposure/routes_test.exs
+++ test/exposure/routes_test.exs
@@ -3,22 +3,27 @@ defmodule Exposure.RoutesTest do
   alias Exposure.Routes
 
   test "get" do
-    assert %{method: :get, path: "/", params: [], func: nil} == Routes.get "/", [], do: nil
+    expected = %{method: :get, path: "/", params: [], func: nil}
+    assert expected == Routes.get "/", [], do: nil
   end
 
   test "post" do
-    assert %{method: :post, path: "/", params: [], func: nil} == Routes.post "/", [], do: nil
+    expected = %{method: :post, path: "/", params: [], func: nil}
+    assert expected == Routes.post "/", [], do: nil
   end
 
   test "put" do
-    assert %{method: :put, path: "/", params: [], func: nil} == Routes.put "/", [], do: nil
+    expected = %{method: :put, path: "/", params: [], func: nil}
+    assert expected == Routes.put "/", [], do: nil
   end
 
   test "patch" do
-    assert %{method: :patch, path: "/", params: [], func: nil} == Routes.patch "/", [], do: nil
+    expected = %{method: :patch, path: "/", params: [], func: nil}
+    assert expected == Routes.patch "/", [], do: nil
   end
 
   test "delete" do
-    assert %{method: :delete, path: "/", params: [], func: nil} == Routes.delete "/", [], do: nil
+    expected = %{method: :delete, path: "/", params: [], func: nil}
+    assert expected == Routes.delete "/", [], do: nil
   end
 end
